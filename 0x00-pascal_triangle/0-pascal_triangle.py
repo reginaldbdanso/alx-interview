9fbc9c4a4baf5945b03a165132b964cf7f5e5a31
reginaldbdanso <reginaldbdanso@gmail.com>
2024-01-03 20:14:00 +0000

    Fixed pycodestyle errors

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,18 +3,19 @@
 Module for Pascals triangle of n
 """
 
+
 def pascal_triangle(n):
     """
-    returns a list of lists of integers representing the Pascals triangle of n   
+    returns a list of lists of integers representing the Pascals triangle of n
     """
     if n <= 0:
         return []
-    
+
     triangle = []
     for i in range(n):
         row = [1] * (i + 1)
         for j in range(1, i):
             row[j] = triangle[i-1][j-1] + triangle[i-1][j]
         triangle.append(row)
-    
+
     return triangle
